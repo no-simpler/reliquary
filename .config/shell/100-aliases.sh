f727c3315775e52ae34ea6366a68976353254eca
Erik Sargazakov <erik@serpnet.org>
2025-05-06 20:53:30 +0200

    Add new ylf shell alias

--- .config/shell/100-aliases.sh
+++ .config/shell/100-aliases.sh
@@ -219,6 +219,7 @@ alias yp='yadm push'
 alias ypt='yadm push --tags'
 alias yff='yadm merge --ff-only @{u}'
 alias ypff='yadm pull --ff-only'
+alias ylf="yadm --no-pager log --format='%w(0,0,7)%C(auto)%h%Creset %<|(56,trunc)%s %C(yellow)(%as)%Creset %C(blue)%<(10,trunc)%al%C(auto)%+d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always | less -R"
 alias ywlf="yadm --no-pager log --format='%w(0,0,7)%C(auto)%h%Creset %<|(116,trunc)%s %C(yellow)(%as)%Creset %C(blue)%<(10,trunc)%al%C(auto)%+d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always | less -R"
 
 ##
