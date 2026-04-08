12626f8f8e1c259c24217c4a3fb9b04daf615012
Erik Sargazakov <erik@serpnet.org>
2026-04-08 13:43:51 +0200

    Move ~/.pbin/ to ~/.config/bin/

--- .config/shell/100-aliases.sh
+++ .config/shell/100-aliases.sh
@@ -90,70 +90,6 @@ zsh) alias bash='/usr/bin/env bash' ;;
 esac
 
 ##
-## Git aliases
-##
-
-alias gf='git fetch --prune --prune-tags --tags --force'
-alias gaa='git add --all'
-alias gaas='git add --all && git status'
-alias grs='git restore --staged'
-alias grc='git rm --cached -rf .'
-alias gs='git status'
-alias gu='git rm --cached -rf . &>/dev/null; git add --all; git status'
-alias gc='git commit'
-alias gp='git push origin -u HEAD'
-alias gpt='git push --tags'
-alias gff='git merge --ff-only @{u}'
-alias glg='git log --all --decorate=full --show-signature'
-
-gbp() {
-    local branch="${1:-main}"
-    git branch --merged "$branch" | grep -v '^\*' | grep -v "^$branch$" | xargs -r git branch -d
-}
-
-# git-log (80-character-wide semi-oneliners)
-alias glfr="git --no-pager log --format='%w(0,0,7)%C(auto)%h%Creset %<|(56,trunc)%s %C(yellow)(%as)%Creset %C(blue)%<(10,trunc)%al%C(auto)%+d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always"
-alias glr="glfr -10"
-alias gllr="glfr -20"
-alias glllr="glfr -40"
-alias gllllr="glfr -80"
-
-# git-log (80-character-wide semi-oneliners) with output re-directed to less
-alias glf="glfr | less -R"
-alias gl="glfr -10 | less -R"
-alias gll="glfr -20 | less -R"
-alias glll="glfr -40 | less -R"
-alias gllll="glfr -80 | less -R"
-
-# git-log (140-character-wide semi-oneliners)
-alias gwlfr="git --no-pager log --format='%w(0,0,7)%C(auto)%h%Creset %<|(116,trunc)%s %C(yellow)(%as)%Creset %C(blue)%<(10,trunc)%al%C(auto)%+d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always"
-alias gwlr="gwlfr -10"
-alias gwllr="gwlfr -20"
-alias gwlllr="gwlfr -40"
-alias gwllllr="gwlfr -80"
-
-# git-log (140-character-wide semi-oneliners) with output re-directed to less
-alias gwlf="gwlfr | less -R"
-alias gwl="gwlfr -10 | less -R"
-alias gwll="gwlfr -20 | less -R"
-alias gwlll="gwlfr -40 | less -R"
-alias gwllll="gwlfr -80 | less -R"
-
-# git-log (unlimited true oneliners)
-alias golfr="git --no-pager log --format='%C(auto)%h%Creset %s %C(yellow)(%as)%Creset %C(blue)%al%C(auto)%d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always"
-alias golr="golfr -10"
-alias gollr="golfr -20"
-alias golllr="golfr -40"
-alias gollllr="golfr -80"
-
-# git-log (unlimited true oneliners) with output re-directed to less
-alias golf="golfr | less -R"
-alias gol="golfr -10 | less -R"
-alias goll="golfr -20 | less -R"
-alias golll="golfr -40 | less -R"
-alias gollll="golfr -80 | less -R"
-
-##
 ## $PATH aliases
 ##
 
@@ -192,42 +128,6 @@ alias du='du -kh -d 1'
 #.  -h  - Human-readable sizes
 ##
 alias df='df -kh'
-
-##
-## Docker
-##
-
-alias dcu='docker compose up -d'
-alias dcb='docker compose up -d --build --no-deps'
-alias dce='docker compose exec'
-alias dcd='docker compose down'
-alias dcdd='docker compose down --volumes --rmi all --remove-orphans'
-alias dcl='docker compose logs -f'
-
-dcex() {
-    docker compose exec \
-        --env XDEBUG_TRIGGER=1 \
-        --env PHP_IDE_CONFIG="serverName=$(basename "$PWD")" \
-        "$@"
-}
-
-##
-## yadm
-##
-
-# Wrap yadm executable to add custom commands to it
-alias yadm=~/.pbin/yadm-wrapper
-alias ya='yadm add'
-alias yf='yadm fetch --prune --prune-tags --tags --force'
-alias yrs='yadm restore --staged'
-alias ys='yadm status'
-alias yc='yadm commit'
-alias yp='yadm push'
-alias ypt='yadm push --tags'
-alias yff='yadm merge --ff-only @{u}'
-alias ypff='yadm pull --ff-only'
-alias ylf="yadm --no-pager log --format='%w(0,0,7)%C(auto)%h%Creset %<|(56,trunc)%s %C(yellow)(%as)%Creset %C(blue)%<(10,trunc)%al%C(auto)%+d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always | less -R"
-alias ywlf="yadm --no-pager log --format='%w(0,0,7)%C(auto)%h%Creset %<|(116,trunc)%s %C(yellow)(%as)%Creset %C(blue)%<(10,trunc)%al%C(auto)%+d%Creset' --all --graph --abbrev=7 --decorate-refs-exclude='refs/tags/publish-*' --color=always | less -R"
 
 ##
 ## zsh-specific
