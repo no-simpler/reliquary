4097ec133d338355022513d9909e635a9ea438c4
Erik Sargazakov <erik@serpnet.org>
2026-04-08 13:46:12 +0200

    Remove unused funcs, add binary markers to gitattributes, update CLAUDE.md

--- .config/shell/090-funcs.sh
+++ .config/shell/090-funcs.sh
@@ -10,22 +10,6 @@ mcd() {
     mkdir -p -- "$1" && cd -- "$1"
 }
 
-#>  cdf
-#
-## Changes into the directory that appears first in the sorted order.
-#
-cdf() {
-    cd $(find . -type d | sort | head -1)
-}
-
-#>  cdl
-#
-## Changes into the directory that appears last in the sorted order.
-#
-cdl() {
-    cd $(find . -type d | sort | tail -1)
-}
-
 #>  tca
 #
 ## Tmux: create or attach to session
@@ -47,23 +31,5 @@ tca() {
     else
         # If the session exists, attach to it
         tmux attach-session -t "$session_name"
-    fi
-}
-
-#>  deidea
-#
-## Removes Intellij IDEA files and directories from all subdirectories of the
-#. current one
-#
-deidea() {
-    if [ "$1" = '-f' ]; then
-        local tmp="$(mktemp)"
-        find . -name '*.iml' -type f | tee -a $tmp | xargs rm -f
-        find . -name '.idea' -type d | tee -a $tmp | xargs rm -rf
-        cat $tmp
-        rm -f $tmp
-    else
-        find . -name '*.iml' -type f
-        find . -name '.idea' -type d
     fi
 }
